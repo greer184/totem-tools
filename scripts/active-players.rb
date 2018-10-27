c8325664bc32a9582da5acfb58e43760c0adce88
William Greer <wgreer184@gmail.com>
2018-10-27 13:03:55 -0400

    Find delegators for an account

--- scripts/active-players.rb
+++ scripts/active-players.rb
@@ -1,1 +1,13 @@
 require 'radiator'
+
+api = Radiator::Api.new
+
+api.get_account_history("qfilter", -1, 10000) do |data|
+    data.each do |i, item|
+        type, op = item.op
+        if type == "delegate_vesting_shares"
+            puts op.delegator
+            puts op.vesting_shares
+        end
+    end
+end
