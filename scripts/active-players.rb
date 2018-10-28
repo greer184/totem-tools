f9adb30ee7cd8e1c02ed48aa93204c816a271b30
William Greer <wgreer184@gmail.com>
2018-10-27 20:23:31 -0400

    Created function to track players

--- scripts/active-players.rb
+++ scripts/active-players.rb
@@ -1,13 +1,55 @@
 require 'radiator'
 
-api = Radiator::Api.new
+def get_player_base(api, totem) 
 
-api.get_account_history("qfilter", -1, 10000) do |data|
-    data.each do |i, item|
-        type, op = item.op
-        if type == "delegate_vesting_shares"
-            puts op.delegator
-            puts op.vesting_shares
+    # Get latest STEEM per VEST number
+    convertion = 0
+    api.get_dynamic_global_properties do |data|
+        steem = data.total_vesting_fund_steem
+        vests = data.total_vesting_shares
+        convertion = steem.to_f / vests.to_f
+    end
+
+    players = []
+    api.get_account_history(totem, -1, 10000) do |data|
+        data.each do |i, item|
+            type, op = item.op
+
+            # Only look for delegation operations.
+            if type == "delegate_vesting_shares"
+
+                # Calculated STEEM delegated to totem.
+                steem = op.vesting_shares.to_f * convertion
+
+                # Create new player
+                if steem >= 0.0     
+                    player = Hash.new
+                    player["name"] = op.delegator
+                
+                    # Update with latest player data.
+                    dup = players.index{|x| op.delegator == x["name"] }
+                    if !dup.nil?
+                        players.delete_at(dup)
+                    end
+                
+                    # Cap voting power at 1 STEEM.
+                    player["vote"] = steem 
+                    if steem > 1.0
+                        player["vote"] = 1.0
+                    end
+
+                    # Store players in array.
+                    if steem > 0.0 
+                        players.push(player)
+                    end
+                end
+            end
         end
     end
+    players
+
 end
+
+# Get players
+api = Radiator::Api.new
+puts get_player_base(api, "statsplit")
--- scripts/vote-counter.rb
+++ scripts/vote-counter.rb
@@ -1,7 +1,8 @@
 require 'radiator'
+require_relative 'active-players'
 
 # Turn this into command line options
-author = 'statsplit'
+totem = 'qfilter'
 permlink = 'steemfield-proposal-1'
 
 tempA = 'greer184'
@@ -12,6 +13,8 @@ approve = "{YES}"
 disapprove = "{NO}"
 
 api = Radiator::Api.new
+players = get_player_base(api, totem)
+puts players
 
 # Weight for and against
 in_favor = 0.0
@@ -20,10 +23,10 @@ against = 0.0
 # Count up votes on the post
 api.get_content_replies(tempA, tempP) do |comments|
     comments.each do |comment|
-        if comment.contains?(disapprove) {
+        if comment.contains?(disapprove)
             in_favor += 5
-        } elsif comment.contain?(disapprove) {
+        elsif comment.contain?(disapprove)
             against += 5
-        } 
+        end
     end
 end
