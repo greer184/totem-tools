8f8acb2cad10655daa9b24cef43f1f1626442b6a
William Greer <wgreer184@gmail.com>
2018-10-28 19:52:27 -0400

    Vote counter should now count votes

--- scripts/vote-counter.rb
+++ scripts/vote-counter.rb
@@ -23,10 +23,13 @@ against = 0.0
 # Count up votes on the post
 api.get_content_replies(tempA, tempP) do |comments|
     comments.each do |comment|
-        if comment.contains?(disapprove)
-            in_favor += 5
-        elsif comment.contain?(disapprove)
-            against += 5
+        voter_index = players.index{|x| comment.author == x["name"]} 
+        if !voter_index.nil?
+            if comment.contains?(approve)
+                in_favor += 1.0 * players[voter_index]["vote"]
+            elsif comment.contain?(disapprove)
+                against += 1.0 * players[voter_index]["vote"]
+            end
         end
     end
 end
